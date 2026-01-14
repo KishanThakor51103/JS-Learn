7af334a82253ec2585b98822fdc6ef72c5b401f3
KishanThakor51103 <kishanthakor995143999@gmail.com>
2026-01-14 09:09:49 +0530

    DOM learn

---
+++ JS 05_iterations/04_high_order_loop.js
@@ -0,0 +1,17 @@
+// for of
+
+const arr = [1,2,3,4,5];
+
+for(const itr of arr)
+{
+    console.log(itr);
+}
+
+const greeting = "Hello world";
+
+for(const itr of greeting)
+{
+    console.log(`Each char is ${itr}`);
+}
+
+
